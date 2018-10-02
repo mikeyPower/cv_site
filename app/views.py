9ab3de668add17ed4ea5a6858b7ac304d7c3dffb
michael <powerm3@tcd.ie>
2018-10-02 22:00:18 +0100

    some more work

--- app/views.py
+++ app/views.py
@@ -1,21 +1,24 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+
+# index view function suppressed for brevity
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Power'}  # fake user
     posts = [  # fake array of posts
-        { 
-            'author': {'nickname': 'John'}, 
-            'body': 'fdsfdsfds' 
+        {
+            'author': {'nickname': 'John'},
+            'body': 'fdsfdsfds'
         },
-        { 
-            'author': {'nickname': 'Susan'}, 
-            'body': 'fdsfdsfds' 
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'fdsfdsfds'
         }
     ]
     return render_template("index.html",
                            title='Home',
                            user=user,
-                           posts=posts)
+posts=posts)
--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/michael/Desktop/footware_shopping/flask"
+set -gx VIRTUAL_ENV "/home/michael/Desktop/cv_site/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
