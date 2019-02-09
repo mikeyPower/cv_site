0d1cc7185fea17838fa9f0dc3e98f9d25a17772c
michael <powerm3@tcd.ie>
2019-02-09 18:37:26 +0000

    fixed form

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, request, Flask
+from flask import render_template, flash, redirect, request, Flask, url_for
 from app import app
 from forms import ContactForm
 
@@ -10,22 +10,26 @@ from forms import ContactForm
 @app.route('/')
 @app.route('/index')
 def index():
+    form = ContactForm()
+    #print("Your in the contact section")
+    return render_template("index.html",form=form)
 
-    return render_template("index.html")
-
-@app.route('/contact', methods=['GET', 'POST'])
+@app.route('/contact1', methods=['GET', 'POST'])
 def contact():
   form = ContactForm()
-
+  print("Your in the contact section")
   if request.method == 'POST':
     if form.validate() == False:
       flash('All fields are required.')
-      return render_template('contact.html', form=form)
+      print("flash")
+      return redirect(url_for('contact'))
+      #return render_template("contact1.html")
     else:
       return 'Form posted.'
 
   elif request.method == 'GET':
-    return render_template('contact.html', form=form)
+      return render_template("contact1.html",form=form)
+    #return redirect(url_for('/'))
 
 
 #Build a scrapper for the quotes section of the site to change it everyday
