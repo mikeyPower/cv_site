2796a3279790c9203985ae45115ea180f20c7372
michael <powerm3@tcd.ie>
2019-02-10 18:09:44 +0000

    form working

--- app/views.py
+++ app/views.py
@@ -12,38 +12,29 @@ from forms import ContactForm
 def index():
     #form = ContactForm()
     #print("Your in the contact section")
-    #return render_template("index.html",form=form)
-     form = ContactForm()
-     print("Your in the contact section")
-     if request.method == 'POST':
-       if form.validate() == False:
-         flash('All fields are required.')
-         print("flash")
-         return redirect(url_for('index',_anchor='contact'))
-         #return render_template("index.html")
-       else:
-         flash('Your message was sent, thank you!')
-         return redirect(url_for('index',_anchor='contact'))
+    return render_template("index.html")
 
-     elif request.method == 'GET':
-         return render_template("index.html",form=form)
 
-@app.route('/contact1', methods=['GET', 'POST'])
-def contact():
-  form = ContactForm()
-  print("Your in the contact section")
-  if request.method == 'POST':
-    if form.validate() == False:
-      flash('All fields are required.')
-      print("flash")
-      return redirect(url_for('contact'))
-      #return render_template("contact1.html")
-    else:
-      return 'Form posted.'
 
-  elif request.method == 'GET':
-      return render_template("contact1.html",form=form)
-    #return redirect(url_for('/'))
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   form = ContactForm()
+
+   if request.method == 'POST':
+        result = request.form
+        for k,v in result.items():
+            print(v)
+            #check if string is empty
+            if not v.strip():
+                flash('All fields are required.')
+                print("flash")
+                return redirect(url_for('index',_anchor='contact'))
+        print("Your message has been sent")
+        flash('Your message was sent, thank you!')
+        return redirect(url_for('index',_anchor='contact'))
+
+   elif request.method == 'GET':
+       return render_template("index.html")
 
 
 #Build a scrapper for the quotes section of the site to change it everyday
