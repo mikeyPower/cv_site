ea9b1612cfd116a6be0aff3cb5e8bbe85b22aeca
michael <powerm3@tcd.ie>
2019-02-11 22:19:33 +0000

    in the process of setting up mail server

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from flask import render_template, flash, redirect, request, Flask, url_for
 from app import app
 from forms import ContactForm
 
+from flask_mail import Mail
+import re
 # index view function suppressed for brevity
 #app = Flask(__name__)
 
@@ -18,23 +20,50 @@ def index():
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
+   mail = Mail(app)
    form = ContactForm()
 
    if request.method == 'POST':
         result = request.form
         for k,v in result.items():
-            print(v)
+            print(k,v)
+            if k == "contactEmail":
+                print("contact email")
+                if valid_email(v) == False:
+                    flash('Email is not correct')
+                    return redirect(url_for('index',_anchor='contact'))
             #check if string is empty
             if not v.strip():
                 flash('All fields are required.')
                 print("flash")
                 return redirect(url_for('index',_anchor='contact'))
         print("Your message has been sent")
+
+        msg = Message(recipients=result["contactEmail"],
+                    sender="powerm3@tcd.ie",
+                      body=result["contactMessage"],
+                      subject=result["contactSubject"])
+
+        mail.send(msg)
+
+
+
         flash('Your message was sent, thank you!')
         return redirect(url_for('index',_anchor='contact'))
 
    elif request.method == 'GET':
        return render_template("index.html")
 
+
+
+
+def valid_email(input_email):
+    email = input_email
+    if re.match("\A(?P<name>[\w\-_]+)@(?P<domain>[\w\-_]+).(?P<toplevel>[\w]+)\Z",email,re.IGNORECASE):
+        return True
+    #print("Email is valid")
+    else:
+        return False
+    #print("Email is invalid")
 
 #Build a scrapper for the quotes section of the site to change it everyday
