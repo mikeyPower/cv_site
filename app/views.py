77371f2d90f57551ad3ecb18a01cbbc3fae71139
michael <powerm3@tcd.ie>
2019-02-27 22:22:34 +0000

    update to python3 and added web scrapper

--- app/views.py
+++ app/views.py
@@ -3,12 +3,19 @@ from flask import render_template, flash, redirect, request, Flask, url_for
 from forms import ContactForm
 
 from flask_mail import Mail, Message
-from email.MIMEText import MIMEText
+#from email.MIMEText import MIMEText
+from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 #from flask.ext.sendmail import Message
 import re
 import smtplib
 import os
+
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
 # index view function suppressed for brevity
 app = Flask(__name__)
 
@@ -26,7 +33,8 @@ MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 def index():
     #form = ContactForm()
     #print("Your in the contact section")
-    return render_template("index.html")
+    quotes = quote()
+    return render_template("index.html",quote=quotes[0],author=quotes[1])
 
 
 
@@ -101,6 +109,39 @@ def valid_email(input_email):
         return False
     #print("Email is invalid")
 
+
+def quote():
+# Set the URL you want to webscrape from
+    url = 'https://www.brainyquote.com/quote_of_the_dayl'
+
+# Connect to the URL
+    response = requests.get(url)
+
+# Parse HTML and save to BeautifulSoup object
+    soup = BeautifulSoup(response.text, "html.parser")
+
+#print(soup)
+
+#quote is in meta tag
+    quote = soup.findAll('meta')
+
+#print(quote)
+
+#find the twtter description as the quote lies in that reference
+    for i in quote:
+        try:
+            if(i.attrs['name']=='twitter:description'):
+            #print(i.attrs['content'])
+                quote_of_the_day = i.attrs['content']
+        except:
+            a = 1
+
+#remove quotes
+    newstr = quote_of_the_day.replace('"', "")
+
+#split string into quote and author
+    spit = newstr.split('-')
+    return spit
 
 
 
